9dba5284970b4e78d3778d2025bfcc615334ae54
Akshat Bamania <akshat.bamania23@gmail.com>
2025-01-03 17:46:37 +0530

    Input Statement

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println ("hello World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println (input.next());
+
     }
 }
+
+
+
+
+
