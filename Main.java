60a40625f6ebacfdc720f325f75472b04f274ea6
Akshat Bamania <akshat.bamania23@gmail.com>
2025-01-05 15:24:26 +0530

    TypeConversion

--- Main.java
+++ Main.java
@@ -1,11 +1,92 @@
 
+import java.util.*;
+
 public class Main{
+   
     public static void main( String[] args ){
         
-        System.out.println ( "****" );
-        System.out.print ( "***\n" );
-        System.out.println ( "**" );
-        System.out.print ( "*" );
+        // System.out.println ( "****" );
+        // System.out.print ( "***\n" );
+        // System.out.println ( "**" );
+        // System.out.print ( "*" );
+        
+
+        //Code to calculate sum
+            // int a = 1;
+            // int b = 2;
+
+            // int sum = a+b;
+
+            // System.out.print(sum);
+
+        //Code to take input 
+
+            // Scanner sc = new Scanner ( System.in );
+            // int a = sc.nextInt();
+            // int b = sc.nextInt();
+
+            // int product = a * b;
+
+            // System.out.println(product);
+
+        //Code to calculte area of a circle
+            /* 
+            Scanner sc = new Scanner( System.in );
+            Double rad = sc.nextDouble();
+
+            Double area = 3.14 * rad * rad ;
+
+            System.out.println(area);
+            */
+
+        // Type conversion 
+
+            // int a = 314 ;
+            // long b ;
+
+            // b = a;
+
+            // // System.out.println(b);
+
+            // Scanner sc = new Scanner(System.in);
+
+            // float input = sc.nextInt();
+            // System.out.print(input);
+
+
+        // Type Casting
+
+            // float a = 99.99f;
+            // int b = (int) a;
+
+            // char a = 'a';
+            // int b = a ;
+
+            // System.out.println(b);
+
+
+        //Type promotion in expression
+
+
+            // short a = 1;
+            // byte b = 2 ;
+            // char c = 'a'; 
+            // byte d = (byte) (a + b + c) ;
+
+            // System.out.println(d);
+
+            // int a = 12;
+            // float b = 2f;
+            // long c = 1;
+            // float d =a + b + c ;
+
+            // System.out.print(d);
+
+            byte b = 12;
+            b =(byte) (b*2);
+
+            System.out.print(b);
+
 
     }
 }
---
+++ Que.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Que {
+
+    public static void main(String args[]){
+
+
+        /*
+        Scanner sc = new Scanner( System.in);
+        float pencil = sc.nextFloat();
+        float pen = sc.nextFloat();
+        float eraser = sc.nextFloat();
+
+        float sum =( pencil + pen + eraser );
+        double price = sum *0.18;
+
+        double ttlPrice = sum + price ;
+
+        // System.out.println(ttlPrice);
+
+        int $ = 24 ;
+        System.out.print($); //No error
+
+         */
+    }
+    
+}
